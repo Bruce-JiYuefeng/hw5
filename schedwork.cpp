ef8488e28b395e0be068434266d052ee4ff1373f
Bruce Li <brucel@usc.edu>
2024-04-11 00:01:29 -0700

    fisrt trial

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,11 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool backtrack(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsAssigned, size_t day, size_t shift);
 
+bool canAssignWorker(const AvailabilityMatrix& avail, vector<size_t>& shiftsAssigned, size_t worker, size_t maxShifts) {
+    return (shiftsAssigned[worker] < maxShifts);
+}
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +41,37 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    size_t days = avail.size();
+    size_t workers = avail[0].size();
 
+    sched.resize(days, vector<Worker_T>(dailyNeed, INVALID_ID));
+    vector<size_t> shiftsAssigned(workers, 0);
 
-
-
+    return backtrack(avail, dailyNeed, maxShifts, sched, shiftsAssigned, 0, 0);
 }
 
+bool backtrack(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftsAssigned, size_t day, size_t shift) {
+    if (day == sched.size()) {
+        return true; 
+    }
+
+    if (shift == dailyNeed) {
+        return backtrack(avail, dailyNeed, maxShifts, sched, shiftsAssigned, day + 1, 0); 
+    }
+
+    for (size_t worker = 0; worker < avail[day].size(); ++worker) {
+        if (avail[day][worker] && canAssignWorker(avail, shiftsAssigned, worker, maxShifts)) {
+            sched[day][shift] = worker;
+            shiftsAssigned[worker]++;
+
+            if (backtrack(avail, dailyNeed, maxShifts, sched, shiftsAssigned, day, shift + 1)) {
+                return true; 
+            }
+
+            shiftsAssigned[worker]--;
+            sched[day][shift] = INVALID_ID;
+        }
+    }
+
+    return false; 
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void generateWords(const string& in, const string& floating, string& current, size_t index, const set<string>& dict, set<string>& result, map<char, int>& floatingCounts, size_t usedFloatingCount);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,43 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<string> result;
+    string current = in;
+    map<char, int> floatingCounts;
+    for (char c : floating) {
+        if (floatingCounts.find(c) == floatingCounts.end()) floatingCounts[c] = 1;
+        else floatingCounts[c]++;
+    }
+    generateWords(in, floating, current, 0, dict, result, floatingCounts, 0);
+    return result;
 }
 
 // Define any helper functions here
+void generateWords(const string& in, const string& floating, string& current, size_t index, const set<string>& dict, set<string>& result, map<char, int>& floatingCounts, size_t usedFloatingCount) {
+    if (index == in.length()) {
+        if (dict.find(current) != dict.end() && usedFloatingCount == floating.size()) {
+            result.insert(current);
+        }
+        return;
+    }
+    
+    if (in[index] != '-') {
+        generateWords(in, floating, current, index + 1, dict, result, floatingCounts, usedFloatingCount);
+    } else {
+        for (char c = 'a'; c <= 'z'; ++c) {
+            bool usedFloating = false;
+            if (floatingCounts.find(c) != floatingCounts.end() && floatingCounts[c] > 0) {
+                usedFloating = true;
+                floatingCounts[c]--;
+                usedFloatingCount++;
+            }
+            current[index] = c;
+            generateWords(in, floating, current, index + 1, dict, result, floatingCounts, usedFloatingCount);
+            current[index] = '-';
+            if (usedFloating) {
+                floatingCounts[c]++;
+                usedFloatingCount--;
+            }
+        }
+    }
+}
