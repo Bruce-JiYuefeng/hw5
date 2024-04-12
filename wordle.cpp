99990adc4fdf933f9216fde6194c17da6464ee6c
Bruce Li <brucel@usc.edu>
2024-04-12 03:31:34 -0700

    complete wordle

--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-void generateWords(const string& in, const string& floating, string& current, size_t index, const set<string>& dict, set<string>& result, map<char, int>& floatingCounts, size_t usedFloatingCount);
+void generateWords(string in, string floating, const set<string>& dict, set<string>& result);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -23,42 +23,37 @@ std::set<std::string> wordle(
 {
     // Add your code here
     set<string> result;
-    string current = in;
-    map<char, int> floatingCounts;
-    for (char c : floating) {
-        if (floatingCounts.find(c) == floatingCounts.end()) floatingCounts[c] = 1;
-        else floatingCounts[c]++;
-    }
-    generateWords(in, floating, current, 0, dict, result, floatingCounts, 0);
+    generateWords(in, floating, dict, result);
     return result;
 }
 
 // Define any helper functions here
-void generateWords(const string& in, const string& floating, string& current, size_t index, const set<string>& dict, set<string>& result, map<char, int>& floatingCounts, size_t usedFloatingCount) {
-    if (index == in.length()) {
-        if (dict.find(current) != dict.end() && usedFloatingCount == floating.size()) {
-            result.insert(current);
+void generateWords(string in, string floating, const set<string>& dict, set<string>& result) {
+    size_t pos = in.find('-');
+    if (pos == string::npos) { 
+        if (dict.find(in) != dict.end()) { 
+            result.insert(in);
         }
         return;
     }
-    
-    if (in[index] != '-') {
-        generateWords(in, floating, current, index + 1, dict, result, floatingCounts, usedFloatingCount);
-    } else {
-        for (char c = 'a'; c <= 'z'; ++c) {
-            bool usedFloating = false;
-            if (floatingCounts.find(c) != floatingCounts.end() && floatingCounts[c] > 0) {
-                usedFloating = true;
-                floatingCounts[c]--;
-                usedFloatingCount++;
+
+    for (size_t i = 0; i < in.length(); ++i) {
+        if(in[i] == '-'){
+            if (!floating.empty()) {
+                char currentChar = floating[0];
+                string nextUnused = floating;
+                nextUnused = nextUnused.substr(1);
+                in[i] = currentChar;
+                generateWords(in, nextUnused, dict, result); 
+            }else {
+                for (char ch = 'a'; ch <= 'z'; ++ch) {
+                    in[i] = ch;
+                    generateWords(in, floating, dict, result); 
+                }
             }
-            current[index] = c;
-            generateWords(in, floating, current, index + 1, dict, result, floatingCounts, usedFloatingCount);
-            current[index] = '-';
-            if (usedFloating) {
-                floatingCounts[c]++;
-                usedFloatingCount--;
-            }
+            in[i] = '-';
         }
-    }
+        
+    } 
 }
+
